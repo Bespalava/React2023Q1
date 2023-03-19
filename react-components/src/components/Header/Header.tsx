88593156770f0ec9d1abfb8baae50681f57d0d2c
ybespalava <ybespalava@gmail.com>
2023-03-19 21:16:34 +0100

    feat: header

---
+++ react-components/src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import styles from './Header.module.scss';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <NavLink to="/" className={styles.link} data-testid="main-link">
+          Главная
+        </NavLink>
+        <NavLink to="/about" className={styles.link} data-testid="about-link">
+          О нас
+        </NavLink>
+      </div>
+    );
+  }
+}
