5ea1d9d076986bb5f2569a04435d8c415fd359e2
ybespalava <ybespalava@gmail.com>
2023-03-19 20:53:37 +0100

    feat: add

---
+++ react-components/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './router/AppRouter';
+
+import './index.css';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
