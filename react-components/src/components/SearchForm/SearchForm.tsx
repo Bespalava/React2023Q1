c59da4e25f79b5e85392108fedc244e68382a978
ybespalava <ybespalava@gmail.com>
2023-03-19 21:21:17 +0100

    feat: search f

---
+++ react-components/src/components/SearchForm/SearchForm.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { SearchFormState } from './types';
+
+import styles from './SearchForm.module.scss';
+
+export default class SearchForm extends Component<Record<string, never>, SearchFormState> {
+  state: SearchFormState = {
+    value: '',
+  };
+
+  componentDidMount() {
+    this.setState({ value: localStorage.getItem('Bespalava-search-query') || '' });
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('Bespalava-search-query', this.state.value);
+  }
+
+  handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    const target = e.target;
+    target && this.setState({ value: target.value });
+  }
+
+  render() {
+    return (
+      <form action="" className={styles.form}>
+        <input
+          name="search"
+          type="text"
+          placeholder="Поиск"
+          className={styles.input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.handleSearch(e)}
+          value={this.state.value}
+        />
+      </form>
+    );
+  }
+}
