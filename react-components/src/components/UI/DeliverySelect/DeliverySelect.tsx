24743e783a4f6ff04505e7b275f541963ccab627
ybespalava <ybespalava@gmail.com>
2023-03-26 23:04:40 +0200

    feat: delivery

---
+++ react-components/src/components/UI/DeliverySelect/DeliverySelect.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { DeliverySelectProps } from './types';
+import { ErrorTypes, FormFieldTypes, ErrorMessages } from '../../../utils/types';
+import ErrorMessage from '../../FormErrorMessage';
+
+import styles from './DeliverySelect.module.scss';
+
+export default class DeliverySelect extends Component<DeliverySelectProps, Record<string, never>> {
+  render() {
+    const { forwardRef, errorsArr, errReset } = this.props;
+    return (
+      <label className="label">
+        <span className="label-text">Тип доставки:</span>
+        <select
+          name={FormFieldTypes.DELIVERY}
+          ref={forwardRef}
+          className={styles.select}
+          onChange={(e) => errReset(e)}
+          defaultValue={'default'}
+        >
+          <option disabled value="default">
+            Выберите тип доставки
+          </option>
+          <option>доставка до двери</option>
+          <option>доставка до постамата</option>
+          <option>самовывоз</option>
+        </select>
+        {errorsArr.includes(ErrorTypes.DELIVERY_REQUIRED) && (
+          <ErrorMessage text={ErrorMessages.DELIVERY_REQUIRED} />
+        )}
+      </label>
+    );
+  }
+}
