ada0d21cc52a619a5e9e1eddb342925a090c45e9
ybespalava <ybespalava@gmail.com>
2023-04-03 00:58:24 +0200

    feat: upd field

--- react-components/src/components/UI/DeliverySelect/DeliverySelect.tsx
+++ react-components/src/components/UI/DeliverySelect/DeliverySelect.tsx
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import { DeliverySelectProps } from './types';
-import { ErrorTypes, FormFieldTypes, ErrorMessages } from '../../../utils/types';
+import React from 'react';
+import { ErrorMessages, FormDataValues } from '../../../utils/types';
 import ErrorMessage from '../../FormErrorMessage';
+import { useController, UseControllerProps } from 'react-hook-form';
 
 import styles from './DeliverySelect.module.scss';
 
-export default class DeliverySelect extends Component<DeliverySelectProps, Record<string, never>> {
-  render() {
-    const { forwardRef, errorsArr, errReset } = this.props;
-    return (
-      <label className="label">
-        <span className="label-text">Тип доставки:</span>
-        <select
-          name={FormFieldTypes.DELIVERY}
-          ref={forwardRef}
-          className={styles.select}
-          onChange={(e) => errReset(e)}
-          defaultValue={'default'}
-        >
-          <option disabled value="default">
-            Выберите тип доставки
-          </option>
-          <option>доставка до двери</option>
-          <option>доставка до постамата</option>
-          <option>самовывоз</option>
-        </select>
-        {errorsArr.includes(ErrorTypes.DELIVERY_REQUIRED) && (
-          <ErrorMessage text={ErrorMessages.DELIVERY_REQUIRED} />
-        )}
-      </label>
-    );
-  }
-}
+const DeliverySelect = (props: UseControllerProps<FormDataValues, 'delivery'>) => {
+  const { field, formState } = useController(props);
+  const { errors } = formState;
+
+  return (
+    <label className="label">
+      <span className="label-text">Тип доставки:</span>
+      <select className={styles.select} {...field}>
+        <option disabled value="default">
+          Выберите тип доставки
+        </option>
+        <option>доставка до двери</option>
+        <option>доставка до постамата</option>
+        <option>самовывоз</option>
+      </select>
+      {errors.delivery && errors.delivery.type === 'validate' && (
+        <ErrorMessage text={ErrorMessages.DELIVERY_REQUIRED} />
+      )}
+    </label>
+  );
+};
+
+export default DeliverySelect;
