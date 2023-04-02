efa822f7441a254cf5056135af7ae694dd1eb241
ybespalava <ybespalava@gmail.com>
2023-04-03 00:27:31 +0200

    feat: upd cardlist

--- react-components/src/components/CardList/CardList.tsx
+++ react-components/src/components/CardList/CardList.tsx
@@ -1,32 +1,22 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import CardItem from '../CardItem';
-import db from '../../assets/data/db.json';
-import { CardData } from '../../utils/types';
+import { CardListProps } from './types';
+import { GuardianResponseItem } from '../../utils/types';
 
 import styles from './CardList.module.scss';
 
-export default class CardList extends Component {
-  generateCards() {
-    return db.map((item: CardData) => {
-      return (
-        <CardItem
-          key={item.id}
-          id={item.id}
-          title={item.title}
-          ingredients={item.ingredients}
-          price={item.price}
-          weight={item.weight}
-          ccal={item.ccal}
-        />
-      );
+const CardList: FC<CardListProps> = ({ dataArr, toggleModal }) => {
+  const generateCards = () => {
+    return dataArr.map((item: GuardianResponseItem) => {
+      return <CardItem key={item.id} data={item} toggleModal={toggleModal} />;
     }) as JSX.Element[];
-  }
+  };
 
-  render() {
-    return (
-      <ul className={styles.list} data-testid="card-list">
-        {this.generateCards()}
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className={styles.list} data-testid="card-list">
+      {generateCards()}
+    </ul>
+  );
+};
+
+export default CardList;
