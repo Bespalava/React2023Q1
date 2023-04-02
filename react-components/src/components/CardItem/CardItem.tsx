84e477bd720d821513706bbb56df29427003f243
ybespalava <ybespalava@gmail.com>
2023-04-02 23:36:17 +0200

    feat: upd

--- react-components/src/components/CardItem/CardItem.tsx
+++ react-components/src/components/CardItem/CardItem.tsx
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { CardItemProps } from './types';
+import { convertDate } from '../../utils/convertDate';
 
 import styles from './CardItem.module.scss';
 
-export default class CardItem extends Component<CardItemProps> {
-  constructor(props: CardItemProps) {
-    super(props);
-  }
-
-  render() {
-    const { id, title, ingredients, price, weight, ccal } = this.props;
-    return (
-      <li className={styles.wrapper}>
-        <div className={styles.img}>
-          <img src={`/img/cards/${id}.jpg`} alt="" />
-        </div>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.ingredients}>{ingredients}</p>
-        <div className={styles.footer}>
-          <div className={styles.left}>
-            <span>{weight} гр.</span>
-            <span>{ccal} ккал</span>
-          </div>
-          <span className={styles.price}>{price} ₽</span>
-        </div>
-        <button type="button" className={styles.button}>
-          Выбрать
+const CardItem: FC<CardItemProps> = (props) => {
+  const { body, headline, shortUrl, standfirst, thumbnail } = props.data.fields;
+  const { webPublicationDate } = props.data;
+  const { toggleModal } = props;
+  return (
+    <li
+      className={styles.wrapper}
+      onClick={() => toggleModal({ body, thumbnail, standfirst, webPublicationDate, shortUrl })}
+    >
+      <div className={styles.img}>
+        <img src={thumbnail} className="img" width="500" height="300" alt="" />
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>{convertDate(webPublicationDate)}</p>
+        <p className={styles.title} dangerouslySetInnerHTML={{ __html: headline }}></p>
+      </div>
+      <div className={styles.btnWrapper}>
+        <button type="button" className={styles.more}>
+          Details <span className={styles.arrow}>➜</span>
         </button>
-      </li>
-    );
-  }
-}
+      </div>
+    </li>
+  );
+};
+
+export default CardItem;
