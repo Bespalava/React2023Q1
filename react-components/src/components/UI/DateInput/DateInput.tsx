cbf283fcfbb83711db7a0b83fb3486971bec1908
ybespalava <ybespalava@gmail.com>
2023-04-03 00:57:13 +0200

    feat: upd field

--- react-components/src/components/UI/DateInput/DateInput.tsx
+++ react-components/src/components/UI/DateInput/DateInput.tsx
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
-import { DateInputProps } from './types';
-import { ErrorTypes, FormFieldTypes, ErrorMessages } from '../../../utils/types';
+import React from 'react';
+import { ErrorMessages, FormDataValues } from '../../../utils/types';
 import ErrorMessage from '../../FormErrorMessage';
+import { useController, UseControllerProps } from 'react-hook-form';
 
 import styles from './DateInput.module.scss';
 
-export default class DateInput extends Component<DateInputProps, Record<string, never>> {
-  render() {
-    const { forwardRef, errorsArr, errReset } = this.props;
-    return (
-      <label className="label">
-        <span className="label-text">Дата доставки:</span>
-        <input
-          className={styles.input}
-          type="date"
-          name={FormFieldTypes.DATE}
-          ref={forwardRef}
-          data-testid="date-input"
-          onChange={(e) => errReset(e)}
-        />
-        {errorsArr.includes(ErrorTypes.DATE_REQUIRED) && (
-          <ErrorMessage text={ErrorMessages.DATE_REQUIRED} />
-        )}
-        {errorsArr.includes(ErrorTypes.DATE_INVALID) && (
-          <ErrorMessage text={ErrorMessages.DATE_INVALID} />
-        )}
-      </label>
-    );
-  }
-}
+const DateInput = (props: UseControllerProps<FormDataValues, 'date'>) => {
+  const { field, formState } = useController(props);
+  const { errors } = formState;
+
+  return (
+    <label className="label">
+      <span className="label-text">Дата доставки:</span>
+      <input className={styles.input} type="date" data-testid="date-input" {...field} />
+      {errors.date && errors.date.type === 'required' && (
+        <ErrorMessage text={ErrorMessages.DATE_REQUIRED} />
+      )}
+      {errors.date && errors.date.type === 'validate' && (
+        <ErrorMessage text={ErrorMessages.DATE_INVALID} />
+      )}
+    </label>
+  );
+};
+
+export default DateInput;
