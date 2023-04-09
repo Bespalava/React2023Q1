214ebfe0dcdb1d086596212be1bc45b2ad7219c7
ybespalava <ybespalava@gmail.com>
2023-04-09 22:21:52 +0200

    feat: comp

--- react-components/src/components/FormCardList/FormCardList.tsx
+++ react-components/src/components/FormCardList/FormCardList.tsx
@@ -1,17 +1,17 @@
-import React, { FC } from 'react';
+import React, { Component } from 'react';
 import { FormCardListProps } from './types';
 import { FormData } from '../../utils/types';
 import FormCardItem from '../FormCardItem';
 
 import styles from './FormCardList.module.scss';
 
-const FormCardList: FC<FormCardListProps> = ({ statesArr }) => {
-  const generateCards = () => {
-    return statesArr.map((el: FormData, idx: number) => {
+export default class FormCardList extends Component<FormCardListProps> {
+  generateCards() {
+    return this.props.statesArr.map((el: FormData, idx: number) => {
       return (
         <FormCardItem
           key={idx}
-          index={statesArr.length - idx}
+          index={this.props.statesArr.length - idx}
           name={el.name}
           date={el.date}
           delivery={el.delivery}
@@ -21,13 +21,13 @@ const FormCardList: FC<FormCardListProps> = ({ statesArr }) => {
         />
       );
     }) as JSX.Element[];
-  };
+  }
 
-  return (
-    <ul className={styles.list} data-testid="form-card-list">
-      {generateCards()}
-    </ul>
-  );
-};
-
-export default FormCardList;
+  render() {
+    return (
+      <ul className={styles.list} data-testid="form-card-list">
+        {this.generateCards()}
+      </ul>
+    );
+  }
+}
