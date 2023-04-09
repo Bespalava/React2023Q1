80f7ad46df0753dfe68622bbf29f319fcb4eb09b
ybespalava <ybespalava@gmail.com>
2023-04-09 23:31:15 +0200

    feat: upd comp

--- react-components/src/pages/Page404/Page404.tsx
+++ react-components/src/pages/Page404/Page404.tsx
@@ -1,14 +1,14 @@
-import React, { FC } from 'react';
+import React, { Component } from 'react';
 
 import styles from './Page404.module.scss';
 
-const Page404: FC = () => {
-  return (
-    <div className={styles.wrapper} data-testid="error-page">
-      <p>Page not found</p>
-      <img src="/img/sad.png" width="341" height="311" alt="" />
-    </div>
-  );
-};
-
-export default Page404;
+export default class Page404 extends Component {
+  render() {
+    return (
+      <div className={styles.wrapper} data-testid="error-page">
+        <p>Page not found</p>
+        <img src="/img/sad.png" width="341" height="311" alt="" />
+      </div>
+    );
+  }
+}
