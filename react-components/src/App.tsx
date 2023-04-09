615f9696be5279b4ddd68d7a2c65b4b458dde0fc
ybespalava <ybespalava@gmail.com>
2023-04-09 22:37:22 +0200

    feat: component

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,16 +1,16 @@
-import React, { FC } from 'react';
+import React, { Component } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 
 import './App.scss';
 
-const App: FC = () => {
-  return (
-    <div className="container">
-      <Header />
-      <Outlet />
-    </div>
-  );
-};
-
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Outlet />
+      </div>
+    );
+  }
+}
