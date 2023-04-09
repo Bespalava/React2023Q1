f1e97235f202567ec2fbaf489b06438409115e93
ybespalava <ybespalava@gmail.com>
2023-04-09 23:32:51 +0200

    feat: upd serv

--- react-components/src/services/GuardianService.ts
+++ react-components/src/services/GuardianService.ts
@@ -1,17 +1,20 @@
+import { Component } from 'react';
 import { GuardianResponse } from '../utils/types';
 import axios from 'axios';
 
 const endpointUrl = 'https://content.guardianapis.com/search';
 const apiKey = process.env.REACT_APP_API_KEY;
 
-export const getData = async (query: string, page = 1): Promise<GuardianResponse> => {
-  try {
-    const response = await axios.get(
-      `${endpointUrl}?q=${query}&show-fields=all&api-key=${apiKey}&page-size=20&page=${page}&order-by=newest`
-    );
-    const data: GuardianResponse = response.data;
-    return data;
-  } catch {
-    throw new Error('Something went wrong');
+export default class GuardianService extends Component {
+  static async getData(query: string, page = 1): Promise<GuardianResponse> {
+    try {
+      const response = await axios.get(
+        `${endpointUrl}?q=${query}&show-fields=all&api-key=${apiKey}&page-size=20&page=${page}&order-by=newest`
+      );
+      const data: GuardianResponse = response.data;
+      return data;
+    } catch {
+      throw new Error('Something went wrong');
+    }
   }
-};
+}
