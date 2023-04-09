31a6432a86f0678da63c455bf72cba1409807303
ybespalava <ybespalava@gmail.com>
2023-04-09 22:08:13 +0200

    feat: fix

--- react-components/src/components/CardItem/CardItem.tsx
+++ react-components/src/components/CardItem/CardItem.tsx
@@ -1,32 +1,36 @@
-import React, { FC } from 'react';
+import React, { Component } from 'react';
 import { CardItemProps } from './types';
 import { convertDate } from '../../utils/convertDate';
 
 import styles from './CardItem.module.scss';
 
-const CardItem: FC<CardItemProps> = (props) => {
-  const { body, headline, shortUrl, standfirst, thumbnail } = props.data.fields;
-  const { webPublicationDate } = props.data;
-  const { toggleModal } = props;
-  return (
-    <li
-      className={styles.wrapper}
-      onClick={() => toggleModal({ body, thumbnail, standfirst, webPublicationDate, shortUrl })}
-    >
-      <div className={styles.img}>
-        <img src={thumbnail} className="img" width="500" height="300" alt="" />
-      </div>
-      <div className={styles.content}>
-        <p className={styles.text}>{convertDate(webPublicationDate)}</p>
-        <p className={styles.title} dangerouslySetInnerHTML={{ __html: headline }}></p>
-      </div>
-      <div className={styles.btnWrapper}>
-        <button type="button" className={styles.more}>
-          Details <span className={styles.arrow}>➜</span>
-        </button>
-      </div>
-    </li>
-  );
-};
+export default class CardItem extends Component<CardItemProps, Record<string, never>> {
+  constructor(props: CardItemProps) {
+    super(props);
+  }
 
-export default CardItem;
+  render() {
+    const { body, headline, shortUrl, standfirst, thumbnail } = this.props.data.fields;
+    const { webPublicationDate } = this.props.data;
+    const { toggleModal } = this.props;
+    return (
+      <li
+        className={styles.wrapper}
+        onClick={() => toggleModal({ body, thumbnail, standfirst, webPublicationDate, shortUrl })}
+      >
+        <div className={styles.img}>
+          <img src={thumbnail} className="img" width="500" height="300" alt="" />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.text}>{convertDate(webPublicationDate)}</p>
+          <p className={styles.title} dangerouslySetInnerHTML={{ __html: headline }}></p>
+        </div>
+        <div className={styles.btnWrapper}>
+          <button type="button" className={styles.more}>
+            Details <span className={styles.arrow}>➜</span>
+          </button>
+        </div>
+      </li>
+    );
+  }
+}
