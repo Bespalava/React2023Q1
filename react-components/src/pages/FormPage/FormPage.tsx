4fc568cfe4ae2fdf985500384182245f788ac267
ybespalava <ybespalava@gmail.com>
2023-04-09 23:29:04 +0200

    feat: upd comp

--- react-components/src/pages/FormPage/FormPage.tsx
+++ react-components/src/pages/FormPage/FormPage.tsx
@@ -1,27 +1,34 @@
-import React, { FC, useState } from 'react';
+import React, { Component } from 'react';
 import Form from '../../components/Form';
 import { FormData } from '../../utils/types';
+import { FormPageState } from './types';
 import FormCardList from '../../components/FormCardList';
 
 import styles from './FormPage.module.scss';
 
-const FormPage: FC = () => {
-  const [formStatesArr, setFormStatesArr] = useState<FormData[] | []>([]);
+export default class FormPage extends Component<Record<string, never>, FormPageState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.setFormState = this.setFormState.bind(this);
+    this.state = {
+      formStatesArr: [],
+    };
+  }
 
-  const setFormState = (newState: FormData) => {
-    setFormStatesArr([newState, ...formStatesArr]);
-  };
+  setFormState(newState: FormData) {
+    this.setState({ formStatesArr: [newState, ...this.state.formStatesArr] });
+  }
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.form} data-testid="form-page">
-        <Form setFormState={setFormState} />
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.form} data-testid="form-page">
+          <Form setFormState={this.setFormState} />
+        </div>
+        <h2>Ваши заказы:</h2>
+        {this.state.formStatesArr.length ? '' : <p>Данные не найдены</p>}
+        <FormCardList statesArr={this.state.formStatesArr} />
       </div>
-      <h2>Ваши заказы:</h2>
-      {formStatesArr.length ? '' : <p>Данные не найдены</p>}
-      <FormCardList statesArr={formStatesArr} />
-    </div>
-  );
-};
-
-export default FormPage;
+    );
+  }
+}
