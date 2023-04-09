b00aff0ad04ee2b3b11cb1e44766802c759e3afc
ybespalava <ybespalava@gmail.com>
2023-04-09 23:26:17 +0200

    feat: upd comp

--- react-components/src/components/UI/TimeCheckbox/TimeCheckbox.tsx
+++ react-components/src/components/UI/TimeCheckbox/TimeCheckbox.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import { FormDataValues } from '../../../utils/types';
-import { useController, UseControllerProps } from 'react-hook-form';
+import React, { Component } from 'react';
+import { TimeCheckboxProps } from './types';
+import { FormFieldTypes } from '../../../utils/types';
 
 import styles from './TimeCheckbox.module.scss';
 
-const TimeCheckbox = (props: UseControllerProps<FormDataValues, 'time'>) => {
-  const { field } = useController(props);
-
-  return (
-    <label className="label">
-      <span className="label-text">Время доставки:</span>
-      <div className={styles.slide}>
-        <label className="label-checkbox" htmlFor="checkbox-call">
-          дневное
-        </label>
-        <input className="slide-checkbox" type="checkbox" id="checkbox-call" {...field} />
-        <label
-          className="custom-checkbox"
-          htmlFor="checkbox-call"
-          data-testid="custom-checkbox"
-        ></label>
-        <label className="label-checkbox" htmlFor="checkbox-call">
-          вечернее
-        </label>
-      </div>
-    </label>
-  );
-};
-
-export default TimeCheckbox;
+export default class TimeCheckbox extends Component<TimeCheckboxProps, Record<string, never>> {
+  render() {
+    const { forwardRef } = this.props;
+    return (
+      <label className="label">
+        <span className="label-text">Время доставки:</span>
+        <div className={styles.slide}>
+          <label className="label-checkbox" htmlFor="checkbox-call">
+            дневное
+          </label>
+          <input
+            className="slide-checkbox"
+            type="checkbox"
+            name={FormFieldTypes.TIME}
+            id="checkbox-call"
+            ref={forwardRef}
+          />
+          <label
+            className="custom-checkbox"
+            htmlFor="checkbox-call"
+            data-testid="custom-checkbox"
+          ></label>
+          <label className="label-checkbox" htmlFor="checkbox-call">
+            вечернее
+          </label>
+        </div>
+      </label>
+    );
+  }
+}
